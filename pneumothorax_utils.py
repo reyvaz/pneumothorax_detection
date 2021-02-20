687a5c717cd9f592b813e9c0e16aeb0cb62e5f3d
reynaldo <graducsb@gmail.com>
2021-02-19 18:48:26 -0700

    updated time meter

--- pneumothorax_utils.py
+++ pneumothorax_utils.py
@@ -6,27 +6,12 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from datetime import datetime, timedelta
 from time import time, strftime, gmtime
 
-def current_time_str(zone_offset = -7):
-    date = datetime.today() + timedelta(hours=zone_offset)
-    suffix = 'PM' if date.hour > 12 else 'AM'
-    return '{:02d}:{:02d} {}'.format(date.hour%12, date.minute, suffix)
-
-def verify_gcs_path(GCS_PATH, renew_url):
-    try: tf.io.gfile.glob(GCS_PATH + '/*')[0]
-    except: 
-        print('GCS path has expired. Follow this link to renew: ' + renew_url)
-        raise Exception('GCS path has expired. Follow this link to renew: ' + renew_url) from None
-    print('GCS path is valid')
-    return None
-
-time_passed = lambda start_time: strftime("%M:%S", gmtime(time() - start_time))
-
-def cm_plot(cm, labels, p_size = 8, cmap = plt.cm.Reds, contrast = 4, 
+def cm_plot(cm, labels, p_size = 8, cmap = plt.cm.Reds, contrast = 4,
             subtitle = '', save_fig = False, fontweight = 'normal'):
     '''
     Plots a heatmap of the confusion matrix
-    
-    Args: 
+
+    Args:
         cm: a confusion matrix array. i.e. a sklearn.metrics.confusion_matrix()
         labels: a list with class labels
         cmap: a color map
@@ -34,13 +19,13 @@ def cm_plot(cm, labels, p_size = 8, cmap = plt.cm.Reds, contrast = 4,
                     values to incrase their color saturation.
         subtitle: a string to print below the title
     '''
-    plot_title = '\nConfusion Matrix\n' + subtitle 
-        
+    plot_title = '\nConfusion Matrix\n' + subtitle
+
     cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     cm_color_vals = np.power(cm_normalized, 1/contrast)
     cm_color_vals = np.ma.masked_where(cm_color_vals == 0, cm_color_vals)
     cmap.set_under(color = 'white')
-    
+
     fig = plt.figure()
     fig.set_size_inches(p_size, p_size)
     ax = fig.add_subplot(111)
@@ -54,10 +39,10 @@ def cm_plot(cm, labels, p_size = 8, cmap = plt.cm.Reds, contrast = 4,
             elif cell_norm > 0 and cell_norm < .01: cell_norm = '< 0.01'
             else: cell_norm = format(cell_norm, '.2f')
             cell_text = '{}\n({})'.format(cm[x,y], cell_norm)
-            ax.annotate(cell_text, xy=(y, x), 
+            ax.annotate(cell_text, xy=(y, x),
                     horizontalalignment='center',
                     fontweight = 'bold' if cm[x,y] > 0 else 'normal', size=12,
-                    verticalalignment='center', 
+                    verticalalignment='center',
                     color='white' if cm_color_vals[x,y] > 0.30 else 'gray')
 
     ax.set_xticks(np.arange(len(labels)))
@@ -70,7 +55,7 @@ def cm_plot(cm, labels, p_size = 8, cmap = plt.cm.Reds, contrast = 4,
         spine.set_edgecolor('#f1f1f1')
 
     title_size = min((p_size+8), 16)
-    plt.title(plot_title, fontweight = 'bold', size=title_size)  
+    plt.title(plot_title, fontweight = 'bold', size=title_size)
     plt.tight_layout()
 
     if save_fig:
@@ -85,7 +70,7 @@ def performance_report(DF, preds_col, neg_thresh = 0.2, pos_thresh = 0.8,
     '''
     report_type: one of 'high_confidence', 'inconclusive', 'total'
     preds_col: (str) name of the column for predicted probabilities
-    metrics: a list of strings with any of the names of the variables created 
+    metrics: a list of strings with any of the names of the variables created
         within the function. i.e. metrics = ['accuracy', 'sensitivity']
     '''
     DF = DF[['label', preds_col]].copy()
@@ -134,7 +119,7 @@ def performance_report(DF, preds_col, neg_thresh = 0.2, pos_thresh = 0.8,
 
     output_df = BDF[['label', preds_col, 'preds_round']]
 
-    if len(metrics) > 0: 
+    if len(metrics) > 0:
         scope = locals()
         metrics_dict = {k: eval(k) for k in metrics}
         return output_df, metrics_dict
@@ -151,3 +136,24 @@ def split_dataset(ds, steps, n_parts = 3):
     return ds_parts
 
 hline = u'\u2500'*83
+
+def current_time_str(zone_offset = -7):
+    date = datetime.today() + timedelta(hours=zone_offset)
+    suffix = 'PM' if date.hour > 12 else 'AM'
+    return '{:02d}:{:02d} {}'.format(date.hour%12, date.minute, suffix)
+
+def verify_gcs_path(GCS_PATH, renew_url):
+    try: tf.io.gfile.glob(GCS_PATH + '/*')[0]
+    except:
+        print('GCS path has expired. Follow this link to renew: ' + renew_url)
+        raise Exception('GCS path has expired. Follow this link to renew: ' + renew_url) from None
+    print('GCS path is valid')
+    return None
+    
+# time_passed = lambda start_time: strftime("%M:%S", gmtime(time() - start_time))
+def time_passed(start_time):
+  secs = time() - start_time
+  if secs < 3600: elapsed = strftime("%M:%S", gmtime(secs))
+  elif secs < 3600*24: elapsed = strftime("%H:%M:%S", gmtime(secs))
+  else: elapsed = '{:.0f} seconds'.format(secs)
+  return elapsed
